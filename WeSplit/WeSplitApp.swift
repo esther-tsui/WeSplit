47145087713875bef82952d46c67555746319927
Esther Tsui <66723850+esther-tsui@users.noreply.github.com>
2023-05-23 15:11:37 +0100

    first commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Wing Hei Tsui on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
